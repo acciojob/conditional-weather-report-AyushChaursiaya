95b106e1fd7a8d134c2ad187aed10676d2ea854d
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-11 14:02:39 +0000

    file

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+import WeatherDisplay from "./WeatherDisplay.js";
 
 const App = () => {
+  // { temperature: 25, conditions: "Sunny" }
+  const [weatherData, setWeatherData] = useState({temperature: 0, conditions: ""});
+
+  const fetchWeatherData = async () => {
+    await new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    })
+
+    setWeatherData({temperature: 25, conditions: "Sunny"});
+  }
+
+  useEffect(() => {
+    fetchWeatherData();
+  }, [])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Weather Report</h1>
+        <WeatherDisplay weather={weatherData} />
     </div>
   )
 }
---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,12 @@
+const WeatherDisplay = ({weather}) => {
+    const {temperature, conditions} = weather;
+
+    const tempreatureColor = temperature > 25 ? "red" : "blue";
+
+    return (<div>
+        <p style={{color: tempreatureColor}}>Temperature: {temperature} </p>
+        <p>Conditions: {conditions} </p>
+    </div>)
+}
+
+export default WeatherDisplay;
