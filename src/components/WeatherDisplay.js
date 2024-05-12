bf7a2175fed8736f591cf5a56753932140366c63
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-12 11:34:25 +0000

    file

--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,14 +1,25 @@
 import React from "react";
 
+// const WeatherDisplay = ({weather}) => {
+//     const {temperature, conditions} = weather;
+
+//     const tempreatureColor = temperature > 20 ? "red" : "blue";
+
+//     return (<div>
+//         <p style={{color: tempreatureColor}}>Temperature: {temperature}°C </p>
+//         <p>Conditions: {conditions} </p>
+//     </div>)
+// }
+
+// export default WeatherDisplay;
+
+// weatherDisplay.js
 const WeatherDisplay = ({weather}) => {
-    const {temperature, conditions} = weather;
-
-    const tempreatureColor = temperature > 20 ? "red" : "blue";
-
-    return (<div>
-        <p style={{color: tempreatureColor}}>Temperature: {temperature}°C </p>
-        <p>Conditions: {conditions} </p>
-    </div>)
+  return (
+    <div>
+        <p>Temperature: <span style={{color: `${weather.temperature>20?'red':'blue'}`}}>{weather.temperature}</span></p>
+        <p>Conditions: {weather.conditions}</p>
+    </div>
+  )
 }
-
-export default WeatherDisplay;
+export default WeatherDisplay
