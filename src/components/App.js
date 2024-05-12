8a3a6541010066751b2595a9394dd0831d60e713
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-05-12 11:38:19 +0000

    file

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,49 @@
 
+// import React, { useEffect, useState } from "react";
+// import './../styles/App.css';
+// import WeatherDisplay from "./WeatherDisplay.js";
+
+// // const App = () => {
+// //   // { temperature: 25, conditions: "Sunny" }
+// //   const [weatherData, setWeatherData] = useState({temperature: 0, conditions: ""});
+
+// //   const fetchWeatherData = async () => {
+// //     await new Promise((resolve) => {
+// //       setTimeout(resolve, 1000);
+// //     })
+
+// //     setWeatherData({temperature: 25, conditions: "Sunny"});
+// //   }
+
+// //   useEffect(() => {
+// //     fetchWeatherData();
+// //   }, [])
+
+// //   return (
+// //     <div>
+// //         <h1>Weather Report</h1>
+// //         <WeatherDisplay weather={weatherData} />
+// //     </div>
+// //   )
+// // }
+
+// // export default App
+
+
 import React, { useEffect, useState } from "react";
 import './../styles/App.css';
-import WeatherDisplay from "./WeatherDisplay.js";
-
+import WeatherDisplay from "./WeatherDisplay";
 const App = () => {
-  // { temperature: 25, conditions: "Sunny" }
-  const [weatherData, setWeatherData] = useState({temperature: 0, conditions: ""});
-
-  const fetchWeatherData = async () => {
-    await new Promise((resolve) => {
-      setTimeout(resolve, 1000);
-    })
-
-    setWeatherData({temperature: 25, conditions: "Sunny"});
-  }
-
+  const [weather, setWeather] = useState({temperature: 20, conditions: "Sunny"});
   useEffect(() => {
-    fetchWeatherData();
+    setWeather({ temperature: 25, conditions: "Sunny" });
   }, [])
-
+  
   return (
     <div>
-        <h1>Weather Report</h1>
-        <WeatherDisplay weather={weatherData} />
+        {/* Do not remove the main div */}
+        <WeatherDisplay weather={weather} />
     </div>
   )
 }
-
-export default App
-
-
+export default App;
